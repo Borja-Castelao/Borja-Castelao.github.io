d6b84903763bfef0011515a39f3ec2deac4c659c
Borja-Castelao <76393301+Borja-Castelao@users.noreply.github.com>
2021-05-17 09:47:02 +0200

    Add files via upload

---
+++ ejer-conversacion/leerFicheroXML.js
@@ -0,0 +1,19 @@
+function gestionarFicheroXML(xmlDoc){
+	alert(xmlDoc)
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		if (i == 2)
+		{
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='izquierda'>" + libros[i].textContent + "</p>"
+		}
+		else
+		{
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='derecha'>" + libros[i].textContent + "</p>"
+
+		}
+        alert("recargo")
+        loadDocA("conver.xml","xml");
+
+}
+    setInterval(CargarFichero,2000);
