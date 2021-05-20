e112dc0aa3794734cbc68c30aa944cbcb548b391
Borja-Castelao <76393301+Borja-Castelao@users.noreply.github.com>
2021-05-20 21:43:54 +0200

    Update leerFicheroXML.js

--- ejer-conversacion/leerFicheroXML.js
+++ ejer-conversacion/leerFicheroXML.js
@@ -1,23 +1,19 @@
 function gestionarFicheroXML(xmlDoc){
 	alert(xmlDoc)
 	let capaVacia = document.querySelector("#ficheroXML")
-	let libros = xmlDoc.querySelectorAll("libro")
-	for(let i=0; i<libros.length; i++)
-		if (i == 2)
+	let P1 = xmlDoc.querySelectorAll("persona1")
+	let P2 = xmlDoc.querySelectorAll("persona2")
+
+	for(let i=0; i<P1.length; i++)
 		{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='izquierda'>" + libros[i].textContent + "</p>"
-		}
-		else
-		{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='derecha'>" + libros[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='persona1'>" + P1[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='persona2'>" + P2[i].textContent + "</p>"
 
 		}
         alert("recargo")
-        loadDocA("conver.xml","xml");
-
 }
-    setInterval(CargarFichero,2000)
 function CargarFichero()
 {
         loadDocA("conver.xml","xml");
 };
+    setInterval(CargarFichero,2000)
