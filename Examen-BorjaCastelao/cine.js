2dabc57f14cbe062f7711e4bd3312e84d06e7ea9
Borja-Castelao <76393301+Borja-Castelao@users.noreply.github.com>
2021-05-21 09:35:39 +0200

    Create cine.js

---
+++ Examen-BorjaCastelao/cine.js
@@ -0,0 +1,15 @@
+function gestionarFicheroXML(xmlDoc){
+	let capaVacia = document.querySelector("#cineXML")
+    capaVacia.innerHTML =""
+	let cine = xmlDoc.querySelectorAll("cine")
+	for(let i=0; i<cine.length; i++)
+		if (i == 2)
+		{
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='exa'>" + cine[i].textContent + "</p>"
+		}
+}
+function CargarFichero()
+{
+        loadDocA("cine.xml","xml")
+}
+    setInterval(CargarFichero,5000)
